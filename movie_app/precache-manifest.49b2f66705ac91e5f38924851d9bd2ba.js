5f5a559c172726ad7c2d4881db269309f5433d0d
Daniel Nestor <danieln.webdev@gmail.com>
2020-04-06 18:45:56 +0100

    First commit

---
+++ movie_app/precache-manifest.49b2f66705ac91e5f38924851d9bd2ba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4788af638b6a747dc53c1437c1b5c55f",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "f11d62601ca1b098b9d8",
+    "url": "/movie_app/static/css/main.c56d0c13.chunk.css"
+  },
+  {
+    "revision": "606ecf216479991926d4",
+    "url": "/movie_app/static/js/2.fcc39e5c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/movie_app/static/js/2.fcc39e5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f11d62601ca1b098b9d8",
+    "url": "/movie_app/static/js/main.d73d7aea.chunk.js"
+  },
+  {
+    "revision": "13f6c02c38aa3348d4ee",
+    "url": "/movie_app/static/js/runtime-main.12506f86.js"
+  },
+  {
+    "revision": "01471e75a31c60276257b052b4c89163",
+    "url": "/movie_app/static/media/Iron_man.01471e75.png"
+  }
+]);
---
+++ todo-list/precache-manifest.ff2f4c20e495ce0665b2adeb174d2b0a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d3858a32d592446c89193cde78ecac6",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "60b8e735f97ae0a4cdb1",
+    "url": "/todo-list/static/css/main.e944f1e0.chunk.css"
+  },
+  {
+    "revision": "58e976403c829ddd770c",
+    "url": "/todo-list/static/js/2.a5e12e10.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/todo-list/static/js/2.a5e12e10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60b8e735f97ae0a4cdb1",
+    "url": "/todo-list/static/js/main.f65a49bf.chunk.js"
+  },
+  {
+    "revision": "e23d7e92878aabb6bb79",
+    "url": "/todo-list/static/js/runtime-main.c78e8f9a.js"
+  },
+  {
+    "revision": "fa65be438fb50a7f3c46a37014c43af1",
+    "url": "/todo-list/static/media/check.fa65be43.png"
+  }
+]);
