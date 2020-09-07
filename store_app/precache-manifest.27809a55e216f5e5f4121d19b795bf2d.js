7f76ee3b4021b5849b4e9efb30d0d0a8790e9280
danielndev <63206357+danielndev@users.noreply.github.com>
2020-09-07 23:23:21 +0100

    Add files via upload

---
+++ store_app/precache-manifest.27809a55e216f5e5f4121d19b795bf2d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5799e5b38f3e236c730575e4844dc4ac",
+    "url": "/store_app/index.html"
+  },
+  {
+    "revision": "ed60cc9f5c6ada39b734",
+    "url": "/store_app/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "bb8e819cb52be03f7e84",
+    "url": "/store_app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ed60cc9f5c6ada39b734",
+    "url": "/store_app/static/js/2.6586bcfc.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/store_app/static/js/2.6586bcfc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb8e819cb52be03f7e84",
+    "url": "/store_app/static/js/main.da838dd7.chunk.js"
+  },
+  {
+    "revision": "93b9abf9ab10eda8b826",
+    "url": "/store_app/static/js/runtime-main.72d31e85.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/store_app/static/media/logo.5d5d9eef.svg"
+  }
+]);
