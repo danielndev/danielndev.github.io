1f0c0eef2573457748568a743d65e21d0f0d5d3a
danielndev <63206357+danielndev@users.noreply.github.com>
2020-09-08 01:30:34 +0100

    Add files via upload

---
+++ movie_app/precache-manifest.4ba7bd5c65e97460171764a1704179b5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "785369730980dd1df081090510efc0a2",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "e9d47bcdba0a3ad513f0",
+    "url": "/movie_app/static/css/main.6c47b3a5.chunk.css"
+  },
+  {
+    "revision": "1716e6726c7ff674b68e",
+    "url": "/movie_app/static/js/2.2c804c07.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/movie_app/static/js/2.2c804c07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9d47bcdba0a3ad513f0",
+    "url": "/movie_app/static/js/main.1b2a932f.chunk.js"
+  },
+  {
+    "revision": "13f6c02c38aa3348d4ee",
+    "url": "/movie_app/static/js/runtime-main.12506f86.js"
+  },
+  {
+    "revision": "01471e75a31c60276257b052b4c89163",
+    "url": "/movie_app/static/media/Iron_man.01471e75.png"
+  }
+]);
--- movie_app/service-worker.js
+++ movie_app/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/movie_app/precache-manifest.49b2f66705ac91e5f38924851d9bd2ba.js"
+  "/movie_app/precache-manifest.4ba7bd5c65e97460171764a1704179b5.js"
 );
 
 self.addEventListener('message', (event) => {
